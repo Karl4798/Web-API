ffc485c7deec00627858f4b47d792785f5fc1fbc
Karl Dicks <17667327@vcconnect.co.za>
2020-10-03 18:14:54 +0200

    Update API to include residents

--- Models/Medication.cs
+++ Models/Medication.cs
@@ -8,11 +8,11 @@ namespace AdMedAPI.Models
 
         [Key]
         public int Id { get; set; }
-        [Required] public string Name { get; set; }
-        [Required] public int Quantity { get; set; }
-        [Required] public string TimeSchedule { get; set; }
-        [Required] public string Notes { get; set; }
-        [Required] public int ResidentId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string TimeSchedule { get; set; }
+        public string Notes { get; set; }
+        public int ResidentId { get; set; }
 
     }
 

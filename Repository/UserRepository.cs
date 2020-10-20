9b0410510fbf0ee104b136f1b5478c4675601a53
Karl Dicks <17667327@vcconnect.co.za>
2020-10-20 17:43:47 +0200

    Update JWT Expiry

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -62,7 +62,7 @@ namespace AdMedAPI.Repository
                             new Claim(ClaimTypes.Name, user.Id.ToString()),
                             new Claim(ClaimTypes.Role, user.Role)
                         }),
-                        Expires = DateTime.UtcNow.AddDays(7),
+                        Expires = DateTime.UtcNow.AddHours(12),
                         SigningCredentials = new SigningCredentials
                             (new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
                     };

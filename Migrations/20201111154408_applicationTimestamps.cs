e24bb6d94d47ac90989e05369043d8f3fade6597
Karl Dicks <karl4798@gmail.com>
2020-11-11 17:45:20 +0200

    update applications

---
+++ Migrations/20201111154408_applicationTimestamps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdMedAPI.Migrations
+{
+    public partial class applicationTimestamps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "Residents",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TimeStamp",
+                table: "Applications",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "Residents");
+
+            migrationBuilder.DropColumn(
+                name: "TimeStamp",
+                table: "Applications");
+        }
+    }
+}
